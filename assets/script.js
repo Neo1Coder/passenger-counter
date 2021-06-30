2869d7b1623ec4b325bdafd050e1c3a9bbad409b
FlexFocus <mustafamire@gmail.com>
2021-06-30 21:41:20 +0100

    added functions for save and increament buttons

--- assets/script.js
+++ assets/script.js
@@ -1,64 +1,8 @@
 
-
-
-// var firstBatch = 5
-// var secondBatch = 7
-// var count = firstBatch + secondBatch
-
-// var myAge = 31
-// var humanDogRatio = 7
-// var myDogAge = myAge * humanDogRatio
-// console.log("myDogAge")
-
-// var bonusPoints = 50
-// console.log(bonusPoints);
-
-// var bonusPoints = bonusPoints + 50
-// console.log(bonusPoints);
-
-// var bonusPoints = bonusPoints - 75
-// console.log(bonusPoints);
-
-// var bonusPoints = bonusPoints + 45
-// console.log(bonusPoints);
-
-// function countdown() {
-//     console.log(5)
-//     console.log(4)
-//     console.log(3)
-//     console.log(2)
-//     console.log(1)
-// }
-
-// countdown()
-
-
-// countdown()
-
-// function chosenNumber() {
-//     console.log(42)
-// }
-
-// chosenNumber()
-
-// var lap1 = 34
-// var lap2 = 33
-// var lap3 = 36
-
-
-
-// function totalLapSum() {
-//     var totalLap = lap1 + lap2 + lap3
-//     console.log(totalLap)
-// }
-
-// totalLapSum()
-
-// document.getElementById("countEl")
-
+document.getElementById("count-el")
+document.getElementById("save-el")
+var saveEl = document.getElementById("save-el")
 var countEl = document.getElementById("count-el")
-
-console.log(countEl)
 
 
 var count = 0
@@ -67,22 +11,19 @@ var count = 0
 function increament() {
 
 
-    count = count + 1
-    countEl.innerText = count
-    console.log(count)
+    count += 1
+    countEl.textContent = count
+
 }
-
-// increament()
-// increament()
-// increament()
-
 
 function save() {
 
+    var register = count + " - "
+    saveEl.textContent += register
 
-    console.log(countEl)
 
 
 }
 
-save()
+
+
