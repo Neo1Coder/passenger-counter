708f91384040dca63e0fbd5c08a7facd83f4dcbc
FlexFocus <mustafamire@gmail.com>
2021-06-30 13:50:23 +0100

    first commit

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+//document.getElementById("count-el").innerText = 5
+
+
+// var firstBatch = 5
+// var secondBatch = 7
+// var count = firstBatch + secondBatch
+
+// var myAge = 31
+// var humanDogRatio = 7
+// var myDogAge = myAge * humanDogRatio
+// console.log("myDogAge")
+
+var bonusPoints = 50
+console.log(bonusPoints);
+
+var bonusPoints = bonusPoints + 50
+console.log(bonusPoints);
+
+var bonusPoints = bonusPoints - 75
+console.log(bonusPoints);
+
+var bonusPoints = bonusPoints + 45
+console.log(bonusPoints);
+
