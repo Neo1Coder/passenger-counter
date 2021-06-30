ed385c19941b3c54f2061cb512bb97b76d87c3a1
FlexFocus <mustafamire@gmail.com>
2021-06-30 17:19:38 +0100

    added functionality to count button

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//document.getElementById("count-el").innerText = 5
+
 
 
 // var firstBatch = 5
@@ -10,15 +10,69 @@
 // var myDogAge = myAge * humanDogRatio
 // console.log("myDogAge")
 
-var bonusPoints = 50
-console.log(bonusPoints);
+// var bonusPoints = 50
+// console.log(bonusPoints);
 
-var bonusPoints = bonusPoints + 50
-console.log(bonusPoints);
+// var bonusPoints = bonusPoints + 50
+// console.log(bonusPoints);
 
-var bonusPoints = bonusPoints - 75
-console.log(bonusPoints);
+// var bonusPoints = bonusPoints - 75
+// console.log(bonusPoints);
 
-var bonusPoints = bonusPoints + 45
-console.log(bonusPoints);
+// var bonusPoints = bonusPoints + 45
+// console.log(bonusPoints);
+
+// function countdown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+// countdown()
+
+
+// countdown()
+
+// function chosenNumber() {
+//     console.log(42)
+// }
+
+// chosenNumber()
+
+// var lap1 = 34
+// var lap2 = 33
+// var lap3 = 36
+
+
+
+// function totalLapSum() {
+//     var totalLap = lap1 + lap2 + lap3
+//     console.log(totalLap)
+// }
+
+// totalLapSum()
+
+// document.getElementById("countEl")
+
+var countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+
+var count = 0
+
+
+function increament() {
+
+
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+}
+
+// increament()
+// increament()
+// increament()
 
